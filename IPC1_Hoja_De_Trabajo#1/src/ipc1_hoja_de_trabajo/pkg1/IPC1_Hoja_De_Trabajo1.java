aed7c3c531cf97f6d7984be60879a11df07e6aa4
Unknown <fuentesricardo@hotmail.es>
2016-12-06 23:50:47 -0600

    Inicio del proyecto
    Se termino la interfaz grafica

---
+++ IPC1_Hoja_De_Trabajo#1/src/ipc1_hoja_de_trabajo/pkg1/IPC1_Hoja_De_Trabajo1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1_hoja_de_trabajo.pkg1;
+
+/**
+ *
+ * @author diana
+ */
+public class IPC1_Hoja_De_Trabajo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interfaz inter=new Interfaz();
+        inter.setVisible(true);
+    }
+    
+}
